a4fd341512fcfcfb631afdefddc718c09e050c28
David Camargo <davidcp90@gmail.com>
2016-06-01 11:57:05 -0500

    json scalatra

--- src/main/scala/com/torre/keplerr/Solarr.scala
+++ src/main/scala/com/torre/keplerr/Solarr.scala
@@ -1,16 +1,26 @@
-package com.torre.keplerr
-
+package com.torre.keplerr	
 import org.scalatra._
+import org.scalatra.json._
+import org.json4s.{DefaultFormats, Formats}
 
-class Solarr extends SolarrStack {
-
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
-  }
-
+class Solarr extends SolarrStack with JacksonJsonSupport {
+	case class Person(id: Int, name: String)
+	//filter to make the content type json
+	before() {
+    		contentType = formats("json")
+ 	 }
+	 get("/"){
+  		PersonData.all
+	}	
+	object PersonData {
+		var all = List(
+			Person(1, "David Camargo"),
+			Person(2, "Amaury Prieto"),
+			Person(3, "Manuel Montes"))
+	}
+	
 }
+// Sets up automatic case class to JSON output serialization, required by
+// the JValueResult trait.
+protected implicit val jsonFormats: Formats = DefaultFormats
+
